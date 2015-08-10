843c83c058123b12ac501f6ce10fed9277c5bae7
John Hatton <hattonjohn@gmail.com>
2015-08-10 08:51:23 -0600

    Have shortcuts use static app.ico instead of that of the ephemeral exe
    using the app.ico in the root folder fixes the problem of losing the shortcut icon when we upgrade, lose the original, and eventually the windows explorer cache loses it. There was another attempt at fixing this by finding all the shortcuts and updating them, but that didn't work in our testing and this seems simpler and more robust. There may be some other reason for the old approach of pointing at the icon of the app itself (e.g. could be a different icon)?

--- src/Squirrel/UpdateManager.ApplyReleases.cs
+++ src/Squirrel/UpdateManager.ApplyReleases.cs
@@ -154,7 +154,14 @@ namespace Squirrel
                     thisRelease.Filename));
 
                 var exePath = Path.Combine(Utility.AppDirForRelease(rootAppDirectory, thisRelease), exeName);
-                var fileVerInfo = FileVersionInfo.GetVersionInfo(exePath);
+
+				//Review: using the app.ico in the root folder fixes the problem of losing the shortcut icon when we 
+				//upgrade, lose the original, and eventually the windows explorer cache loses it.
+				//There was another attempt at fixing this by finding all the shortcuts and updating them, but that didn't work in our testing and this seems simpler and more robust.
+				//There may be some other reason for the old approach of pointing at the icon of the app itself (e.g. could be a different icon)?
+				var iconPath = Path.Combine(Utility.AppDirForRelease(rootAppDirectory, thisRelease), "app.ico");
+
+				var fileVerInfo = FileVersionInfo.GetVersionInfo(exePath);
 
                 foreach (var f in (ShortcutLocation[]) Enum.GetValues(typeof(ShortcutLocation))) {
                     if (!locations.HasFlag(f)) continue;
@@ -179,7 +186,7 @@ namespace Squirrel
 
                         sl = new ShellLink {
                             Target = updateExe,
-                            IconPath = exePath,
+                            IconPath = iconPath,
                             IconIndex = 0,
                             WorkingDirectory = Path.GetDirectoryName(exePath),
                             Description = zf.Description,
