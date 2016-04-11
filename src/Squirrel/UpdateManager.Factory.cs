6fe3710cc97f9ec04bc22f8f7cce67548601597f
DALLAS\Thomson <john_thomson@sil.org>
2016-04-11 16:14:35 -0500

    Attempt to get VS2015 AND TC able to build

--- src/Squirrel/UpdateManager.Factory.cs
+++ src/Squirrel/UpdateManager.Factory.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Reflection;
 using System.Runtime.Serialization;
